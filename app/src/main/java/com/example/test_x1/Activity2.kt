a3dae1d3794c675fe0f2b6ebeaf229bb445c5cb3
Remygd <remygarcia20@gmail.com>
2025-09-04 19:38:04 -0600

    Versión completa

--- app/src/main/java/com/example/test_x1/Activity2.kt
+++ app/src/main/java/com/example/test_x1/Activity2.kt
@@ -1,17 +1,46 @@
 package com.example.test_x1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        val nameR = intent.getStringExtra("name")
-        val tName : TextView = findViewById(R.id.textView)
+        val name = intent.getStringExtra("name") // Recuperamos los valores que se mandaron en la anterior actividad
+        val apellido = intent.getStringExtra("apellido")
 
-        tName.text = "Bienvenido $nameR"
+        val txtEdad: EditText = findViewById(R.id.txt_edad) // Referenciamos los cambos con cada cosa
+        val txtCiudad: EditText = findViewById(R.id.txt_ciudad)
+        val btnEnviar: Button = findViewById(R.id.btn_siguiente)
+
+        btnEnviar.setOnClickListener { // Al hacer clic recoge los datos y hace la actividad3 o la siguiente ventana
+            val edad = txtEdad.text.toString().trim()
+            val ciudad = txtCiudad.text.toString().trim()
+
+            if(edad.isEmpty()) {
+                txtEdad.error = "Ingresa tu edad"
+                txtEdad.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(ciudad.isEmpty()) {
+                txtCiudad.error = "Ingresa tu ciudad"
+                txtCiudad.requestFocus()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, Activity3::class.java).apply { //Manda todos los datos que escribimos a la ultima ventana para mostrarnoslos
+                putExtra("name", name)
+                putExtra("apellido", apellido)
+                putExtra("edad", edad)
+                putExtra("ciudad", ciudad)
+            }
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/test_x1/MainActivity.kt
+++ app/src/main/java/com/example/test_x1/MainActivity.kt
@@ -1,29 +1,42 @@
 package com.example.test_x1
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnEnviar : Button = findViewById(R.id.btn_comenzar)
-        val txtName : EditText = findViewById(R.id.txt_name)
+        val btnEnviar: Button = findViewById(R.id.btn_comenzar) //Referenciamos las cosas
+        val txtName: EditText = findViewById(R.id.txt_name)
+        val txtApellido: EditText = findViewById(R.id.txt_apellido)
 
-        btnEnviar.setOnClickListener (){
-            val name = txtName.text.toString()
-            val intent = Intent(this,Activity2::class.java).apply{
-                putExtra("name", name)
+        btnEnviar.setOnClickListener {
+            val name = txtName.text.toString().trim() // Al dar clic vamos a la siguiente actividad y pasamos el nombre y apellido
+            val apellido = txtApellido.text.toString().trim() // Con trim elimina los espacios en blanco
+
+            if(name.isEmpty()) { // Comprueba si está vacio
+                txtName.error = "Ingresa tu nombre" // Envia un error si si
+                txtName.requestFocus()  // Apunta al campo que falta de contestar
+                return@setOnClickListener // Se regresa y ya no ejecuta el codigo si hay error
+            }
+
+            if(apellido.isEmpty()) {
+                txtApellido.error = "Ingresa tu apellido"
+                txtApellido.requestFocus()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this, Activity2::class.java).apply { // Nos manda a la siguiente ventana o Actividad2
+                putExtra("name", name) // Pasamos los datos
+                putExtra("apellido", apellido)
             }
             startActivity(intent)
-            //funcionando
         }
 
     }
-
-
 }
